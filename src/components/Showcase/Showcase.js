a5cbca6735c86d966a0a56a73e0f8cb4b84f0ef7
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-07-12 18:50:00 -0500

    finished showcase

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -53,16 +53,50 @@ const Showcase = () => {
         {/* MIDDLE SECTION RIGHT */}
         <div className="featured__products-middle-right">
             <div className="featured__products-middle-banner-right">
-                <p>SLEEP TIGHT</p>
-                <h1>Crib<br></br>Bedding</h1>
+                <p>Cozy Comfortable</p>
+                <h1>Children's<br></br>Furniture</h1>
                 <img src={images.stardiv} alt="..." />
-                <h2>Make sure your little one <br></br> catches all the Z's</h2>
+                <h2>Unwind and relax<br></br>after playtime</h2>
                 <button className='btn__middle-red'>Browse All</button>
             </div>
             <img src={images.furniture} alt="" />
         </div>
 
         {/* BOTTOM SECTION */}
+
+        <div className="benefits__container">
+            <div className="benefits__container-heading">
+                <h1>Beautiul Beginnings</h1>
+                <h2>customer benefits</h2>
+            </div>
+            <div className="benefits__container-content">
+                <div className="benefits__container-content-card">
+                    <img src={images.stroller} alt="..." />
+                    <h1>10% OFF Registery</h1>
+                    <img src={images.stardiv} alt="..." />
+                    <h2>when you enroll with us</h2>
+                </div>
+                <div className="benefits__container-content-card">
+                    <img src={images.box} alt="..." />
+                    <h1>Free Shipping</h1>
+                    <img src={images.stardiv} alt="..." />
+                    <h2>On all orders over $100</h2>
+                </div>
+                <div className="benefits__container-content-card">
+                    <img src={images.gift} alt="..." />
+                    <h1>Free Gift Wrapping</h1>
+                    <img src={images.stardiv} alt="..." />
+                    <h2>On all gift purchases</h2>
+                </div>
+            </div>
+        </div>
+
+        {/* FINAL IMAGES */}
+
+        <div className="final__img-container">
+            <img src={images.babiesleft} alt="..." />
+            <img src={images.babiesright} alt="..." />
+        </div>
     </>
   )
 }
--- src/constants/images.js
+++ src/constants/images.js
@@ -12,6 +12,12 @@ import crib from '../assets/crib.jpg'
 import fox from '../assets/fox.jpg'
 import cribbedding from '../assets/cribbedding.png'
 import furniture from '../assets/furniture.png'
+import box from '../assets/BB-box-graphic.png'
+import gift from '../assets/BB-gift-graphic.png'
+import stroller from '../assets/BB-stroller-graphic.png'
+import babiesleft from '../assets/welovebabies-left.png'
+import babiesright from '../assets/welovebabies-right.png'
+
 
 export default {
     phone,
@@ -28,4 +34,9 @@ export default {
     fox,
     cribbedding,
     furniture,
+    box,
+    gift,
+    stroller,
+    babiesleft,
+    babiesright,
 }
