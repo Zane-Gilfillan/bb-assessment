d02705012d9a5508d3b225ae0a85ad5121d886cf
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-07-13 00:42:47 -0500

    setting up footer responsiveness for mobile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Footer.scss'
 import { images } from '../../constants'
+import { AiOutlinePlus, AiOutlineMinus} from 'react-icons/ai'
 
 const Footer = () => {
   return (
@@ -8,43 +9,68 @@ const Footer = () => {
         {/* MAIN FOOTER */}
         <div className="footer__container">
             <div className="footer__container-card">
-                <h1>Shop</h1>
-                <a href=""><p>baby apparel</p></a>
-                <a href=""><p>baby gifts</p></a>
-                <a href=""><p>furniture</p></a>
-                <a href=""><p>bedding</p></a>
-                <a href=""><p>lighting</p></a>
-                <a href=""><p>sale</p></a>
+                <div className="footer__container-card-header">
+                    <h1>Shop</h1>
+                    <AiOutlinePlus />
+                </div>
+                <div className="footer__container-card-list">
+                    <a href=""><p>baby apparel</p></a>
+                    <a href=""><p>baby gifts</p></a>
+                    <a href=""><p>furniture</p></a>
+                    <a href=""><p>bedding</p></a>
+                    <a href=""><p>lighting</p></a>
+                    <a href=""><p>sale</p></a>
+                </div>
             </div>
+
             <div className="footer__container-card">
-                <h1>Service</h1>
-                <a href=""><p>contact us</p></a>
-                <a href=""><p>order status</p></a>
-                <a href=""><p>shipping policy</p></a>
-                <a href=""><p>return policy</p></a>
-                <a href=""><p>my account</p></a>
-                <a href=""><p>faq</p></a>
+                <div className="footer__container-card-header">
+                    <h1>Service</h1>
+                    <AiOutlinePlus />
+                </div>
+                <div className="footer__container-card-list">
+                    <a href=""><p>contact us</p></a>
+                    <a href=""><p>order status</p></a>
+                    <a href=""><p>shipping policy</p></a>
+                    <a href=""><p>return policy</p></a>
+                    <a href=""><p>my account</p></a>
+                    <a href=""><p>faq</p></a>
+                </div>
             </div>
+
             <div className="footer__container-card">
-                <h1>About</h1>
-                <a href=""><p>our story</p></a>
-                <a href=""><p>baby registry</p></a>
-                <a href=""><p>store location</p></a>
-                <a href=""><p>reviews</p></a>
-                <a href=""><p>news</p></a>
-                <a href=""><p>subscribe</p></a>
+                <div className="footer__container-card-header">
+                    <h1>About</h1>
+                    <AiOutlinePlus />
+                </div>
+
+                <div className="footer__container-card-list">
+                    <a href=""><p>our story</p></a>
+                    <a href=""><p>baby registry</p></a>
+                    <a href=""><p>store location</p></a>
+                    <a href=""><p>reviews</p></a>
+                    <a href=""><p>news</p></a>
+                    <a href=""><p>subscribe</p></a>
+                </div>
             </div>
+
             <div className="footer__container-card">
-                <h1>Contact</h1>
+                <div className="footer__container-card-header">
+                    <h1>Contact</h1>
+                    <AiOutlinePlus />
+                </div>
+                <div className="footer__container-card-list">
                 <a href=""><p>4472 Lawn Avenue<br></br><br></br>Western Springs, IL 60658</p></a>
                 <a href=""><p>1-708-246-1212</p></a>
                 <a href=""><p>Store Hours</p></a>
-                <div className="footer__container-card-icons">
-                    <i className='facebook'><img src={images.facebook} alt="" /></i>
-                    <i className='twitter'><img src={images.twitter} alt="" /></i>
-                    <i><img className='instagram' src={images.instagram} alt="" /></i>
-                    <i><img className='tiktok' src={images.tiktok} alt="" /></i>
-                </div>
+                    <div className="footer__container-card-icons">
+                        <i className='facebook'><img src={images.facebook} alt="" /></i>
+                        <i className='twitter'><img src={images.twitter} alt="" /></i>
+                        <i><img className='instagram' src={images.instagram} alt="" /></i>
+                        <i><img className='tiktok' src={images.tiktok} alt="" /></i>
+                    </div>
+                    </div>
+
             </div>
         </div>
 
